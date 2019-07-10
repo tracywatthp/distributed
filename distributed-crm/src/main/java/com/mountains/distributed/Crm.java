09e900840cd327257950351d46358eb230d6b7ef
tinxi <tracywatthp@126.com>
2019-07-10 10:47:07 +0800

    配置eureka注册中心并将服务注册到注册中心

--- distributed-crm/src/main/java/com/mountains/distributed/Crm.java
+++ distributed-crm/src/main/java/com/mountains/distributed/Crm.java
@@ -3,9 +3,11 @@ package com.mountains.distributed;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan({"com.mountains.distributed.**.mapper"}) // mapper扫描路径
+@EnableEurekaClient  //本服务启动后会自动注册进eureka服务中
 public class Crm {
 
 	public static void main(String[] args) {
